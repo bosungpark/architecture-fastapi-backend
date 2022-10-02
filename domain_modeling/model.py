c502c101ddfffd28082fe3c71caf3269b29cb960
bosung park <81157873+bosungpark@users.noreply.github.com>
2022-10-02 18:38:21 +0900

    feat: batch modeling

---
+++ domain_modeling/model.py
@@ -0,0 +1,44 @@
+"""
+domain model is mental map of that business owner have of their business
+"""
+from dataclasses import dataclass
+from datetime import date
+from typing import Optional,NewType
+
+Quantity=NewType("Quantity", int)
+SKU=NewType("SKU",str)
+Reference=NewType("Reference",str)
+
+
+@dataclass(frozen=True)
+class OrderLine:
+    orderid:str
+    sku: str
+    qty: int
+
+class Batch:
+    def __init__(self, ref: Reference, sku: SKU, qty: Quantity, eta: Optional[date]):
+        self.reference=ref
+        self.sku=sku
+        self.eta=eta
+        self._purchased_quantity=qty
+        self._allocations=set()
+
+    def allocate(self, line:OrderLine):
+        if self.can_allocate(line):
+            self._allocations.add(line)
+
+    def deallocate(self, line:OrderLine):
+        if self.can_allocate(line):
+            self._allocations.remove(line)
+
+    @property
+    def allocated_quantity(self)->int:
+        return sum(line.qty for line in self._allocations)
+
+    @property
+    def available_quantity(self) -> int:
+        return self._purchased_quantity-self.allocated_quantity
+
+    def can_allocate(self,line:OrderLine)->bool:
+        return self.sku==line.sku and self.available_quantity>=line.qty
