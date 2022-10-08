3d8660c97486eb0c6624a5c45b184704bb142ff3
bosung park <81157873+bosungpark@users.noreply.github.com>
2022-10-08 23:22:42 +0900

    Feat: handel exception

--- service_layer_pattern/src/allocation/entrypoints/fast_api.py
+++ service_layer_pattern/src/allocation/entrypoints/fast_api.py
@@ -5,31 +5,43 @@ service layer pattren
 separate between orchrstration logic and business logic
 : entry-point to domain model
 """
+from typing import List
 
 from fastapi import FastAPI
+from fastapi.openapi.models import Response
 from fastapi.params import Body
 
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, Session
 from starlette import status
 
 from src.allocation.adapters.orm import start_mappers
 from src.allocation.adapters.repository import SqlAlchemyRepository
 from src.allocation.config import get_postgres_uri
-from src.allocation.domain.model import OrderLine, allocate
+from src.allocation.domain.model import OrderLine, allocate, Batch, SKU, Reference, OutOfStock
 
 start_mappers()
 get_session = sessionmaker(bind=create_engine(get_postgres_uri()))
 app = FastAPI()
 
-@app.post("/allocate", status_code=status.HTTP_201_CREATED)
-def allocate_endpoint(data=Body()) -> dict:
-    session= get_session()
-    batches = SqlAlchemyRepository(session).list()
+def is_valid_sku(sku: SKU, batches: List[Batch]):
+    return sku in {b.sku for b in batches}
+
+@app.post("/allocate", status_code=201)
+def allocate_endpoint(data=Body(), response=Response) -> dict:
+    session : Session = get_session()
+    batches : List[Batch] = SqlAlchemyRepository(session).list()
     line = OrderLine(
         data["orderid"],
         data["sku"],
         data["qty"]
     )
-    batchref = allocate(line,batches)
-    return {"batchref":batchref}
+    if not is_valid_sku(line.sku, batches):
+        return {"message":f"Invalid sku {line.sku}","status_code":400}
+    try:
+        batchref : Reference = allocate(line,batches)
+    except OutOfStock as e:
+        return {"message": str(e),"status_code":400}
+    else:
+        session.commit()
+        return {"batchref" : batchref}
--- service_layer_pattern/tests/e2e/test_api.py
+++ service_layer_pattern/tests/e2e/test_api.py
@@ -38,3 +38,48 @@ def test_api_returns_allocation(add_stock):
     r= requests.post(url=f"{url}/allocate", json=data)
     assert r.status_code== 201
     assert r.json()["batchref"] == earlybatch
+
+@pytest.mark.usefixtures("restart_api")
+def test_api_returns_are_persisted(add_stock):
+    sku=random_sku()
+    batch1, batch2 = random_batchref(1), random_batchref(2)
+    order1, order2 = random_orderid(1), random_orderid(2)
+    add_stock([
+        (batch1, sku, 100, "2022-01-01"),
+        (batch2, sku, 100, "2022-01-02"),
+    ])
+    # _____save_____
+    line1={"orderid": order1, "sku": sku, "qty": 100}
+    line2 = {"orderid": order2, "sku": sku, "qty": 100}
+    url= config.get_api_url()
+
+    r= requests.post(url=f"{url}/allocate", json=line1)
+    assert r.status_code== 201
+    assert r.json()["batchref"] == batch1
+
+    r = requests.post(url=f"{url}/allocate", json=line2)
+    assert r.status_code == 201
+    assert r.json()["batchref"] == batch2
+
+@pytest.mark.usefixtures("restart_api")
+def test_400_for_out_of_stock(add_stock):
+    sku, smalll_batch, large_order= random_sku(), random_batchref(), random_orderid()
+    add_stock([
+        (smalll_batch, sku, 10, None)
+    ])
+    data={"orderid": large_order, "sku": sku, "qty": 100}
+    url = config.get_api_url()
+
+    r = requests.post(url=f"{url}/allocate", json=data)
+    assert r.json()["status_code"] == 400
+    assert r.json()["message"] == f"out of stock sku {sku}"
+
+@pytest.mark.usefixtures("restart_api")
+def test_400_for_invalid_sku(add_stock):
+    unknown_sku, orderid= random_sku(), random_orderid()
+    data={"orderid": orderid, "sku": unknown_sku, "qty": 100}
+    url = config.get_api_url()
+
+    r = requests.post(url=f"{url}/allocate", json=data)
+    assert r.json()["status_code"] == 400
+    assert r.json()["message"] == f"Invalid sku {unknown_sku}"
